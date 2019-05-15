56d1e9dcd31a2204ad5cbbb1822dd7f6be6dab7d
Tangerinez <gtang159@gmail.com>
2019-05-15 16:48:22 -0700

    Added spotify api functionality in the CLI. Also updated some of the code to ES6, specifically the template literals and also the default parameter values.

--- liri.js
+++ liri.js
@@ -1,29 +1,28 @@
 require("dotenv").config();
 
-const keys = require("./keys.js"); // To have access to our spotify api keys
-const axios = require("axios"); // Need to get information using axios
-const moment = require("moment"); // Formatting dates in node
+// Modules
+const keys = require("./keys.js");
+const axios = require("axios");
+const SpotifyAPI = require("node-spotify-api");
+const fs = require("fs");
+const moment = require("moment");
 moment().format();
-const fs = require("fs"); // Reading out text file
-const spotifyAPI = require("node-spotify-api"); // Having access to spotify api and using the keys to grab information
-/* const spotify = new Spotify(keys.spotify); */
 
-let userCommand = process.argv[2];
-let query = process.argv[3];
+const spotify = new SpotifyAPI(keys.spotify);
+const userCommand = process.argv[2];
+const query = process.argv[3];
 
 switch (
   userCommand // We can use a switch statement to check for the user's command input in the command line!
 ) {
-  case "concert-this":
-    console.log("NOT DONE YET");
+  case "concert-this": // info about concerts for a certain band
     concerts(query);
     break;
-  case "spotify-this-song":
-    console.log("NOT DONE YET");
+  case "spotify-this-song": // info about songs with a specific song name
+    spotifyThisSong(query);
     // Displays information about a song
     break;
-  case "movie-this":
-    console.log("NOT DONE YET");
+  case "movie-this": // info about movie with a specific name
     movieInfo(query);
     break;
   case "do-what-it-says":
@@ -33,7 +32,8 @@ switch (
 }
 
 // FUNCTION for userCommand "concert-this" //
-function concerts(query) {
+function concerts(query = "Metallica") {
+  // ES6 way of declaring parameter value if it is undefined
   axios
     .get(
       "https://rest.bandsintown.com/artists/" +
@@ -43,14 +43,10 @@ function concerts(query) {
     .then(function(response) {
       for (let i = 0; i < response.data.length; i++) {
         const concert = response.data[i];
-        let concertInfo =
-          "--------------------------------------------------------------------" +
-          "\nName of the venue: " +
-          concert.venue.name +
-          "\nVenue Location: " +
-          concert.venue.city +
-          "\nDate of the Event: " +
-          moment(concert.datetime).format("L"); // formats the date of the concert
+        let concertInfo = `--------------------------------------------------------------------
+Name of the venue: ${concert.venue.name} 
+Venue Location: ${concert.venue.city} 
+Date of the Event: ${moment(concert.datetime).format("L")}`;
         console.log(concertInfo);
       }
     })
@@ -60,38 +56,47 @@ function concerts(query) {
 }
 
 // FUNCTION for userCommand "movie-this" //
-function movieInfo(query) {
-  if (query === undefined) {
-    query = "mr nobody";
-  }
+function movieInfo(query = "mr nobody") {
+  // ES6 way of declaring parameter value if it is undefined
   axios
     .get(
-      "https://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=trilogy"
+      "https://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=trilogy" // grabbing from omdb api
     )
     .then(function(response) {
       const movie = response.data;
-      let movieInformation =
-        "--------------------------------------------------------------------" +
-        "\nTitle of the movie: " +
-        movie.Title +
-        "\nYear the movie came out: " +
-        movie.Year +
-        "\nIMDB Rating of the movie: " +
-        movie.imdbRating /*
-        "\nRotten Tomatoes Rating of the movie: " +
-        movie.Ratings[1].Value + */ +
-        "\nCountry where the movie was produced: " +
-        movie.Country +
-        "\nLanguage of the movie: " +
-        movie.Language +
-        "\nPlot of the movie: " +
-        movie.Plot +
-        "\nActors/Actresses in the movie: " +
-        movie.Actors;
-      console.log(response);
+      let movieInformation = `--------------------------------------------------------------------
+Title of the movie: ${movie.Title} 
+Year the movie came out: ${movie.Year} 
+IMDB Rating of the movie: ${movie.imdbRating} 
+Rotten Tomatoes Rating of the movie: ${movie.Ratings[1].Value} 
+Country where the movie was produced: ${movie.Country}
+Language of the movie: ${movie.Language}
+Plot of the movie: ${movie.Plot} 
+Actors/Actresses in the movie: ${movie.Actors}`;
       console.log(movieInformation);
     })
     .catch(function(error) {
       console.log(error);
+    });
+}
+
+// FUNCTION for userCommand "spotify-this-song" //
+function spotifyThisSong(songQuery = "The Sign") {
+  // ES6 way of declaring parameter value if it is undefined
+  spotify
+    .search({ type: "track", query: songQuery }) // node-spotify-api documentation for retrieving information about a song
+    .then(function(response) {
+      for (let i = 0; i < 3; i++) {
+        const song = response.tracks.items[i];
+        let spotifySong = `--------------------------------------------------------------------
+Artist(s): ${song.artists[0].name} 
+Song Name: ${song.name}
+Album Name:  ${song.album.name} 
+Preview Link:  ${song.preview_url}`;
+        console.log(spotifySong);
+      }
+    })
+    .catch(function(err) {
+      console.log(err);
     });
 }
---
+++ test.js
@@ -0,0 +1,5 @@
+let spotifySong = `--------------------------------------------------------------------
+          Artist(s): ${song.artists[0].name} 
+          Song Name: ${song.name}
+          Album Name:  ${song.album.name} 
+          Preview Link:  ${song.preview_url}`;
