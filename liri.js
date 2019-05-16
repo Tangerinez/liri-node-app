d41000cb98b523e58a4410a3be0e660a7dd6d00e
Tangerinez <gtang159@gmail.com>
2019-05-16 11:22:45 -0700

    Added appendfile functionality to a log.txt file

--- liri.js
+++ liri.js
@@ -48,6 +48,11 @@ Name of the venue: ${concert.venue.name}
 Venue Location: ${concert.venue.city} 
 Date of the Event: ${moment(concert.datetime).format("L")}`;
         console.log(concertInfo);
+        fs.appendFile("log.txt", concertInfo, function(err) {
+          if (err) {
+            console.log(err);
+          }
+        });
       }
     })
     .catch(function(error) {
@@ -74,6 +79,11 @@ Language of the movie: ${movie.Language}
 Plot of the movie: ${movie.Plot} 
 Actors/Actresses in the movie: ${movie.Actors}`;
       console.log(movieInformation);
+      fs.appendFile("log.txt", movieInformation, function(err) {
+        if (err) {
+          console.log(err);
+        }
+      });
     })
     .catch(function(error) {
       console.log(error);
@@ -94,6 +104,11 @@ Song Name: ${song.name}
 Album Name:  ${song.album.name} 
 Preview Link:  ${song.preview_url}`;
         console.log(spotifySong);
+        fs.appendFile("log.txt", spotifySong, function(err) {
+          if (err) {
+            console.log(err);
+          }
+        });
       }
     })
     .catch(function(err) {
